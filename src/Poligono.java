050defa4e8327cba295b9f2eb32e95ffc474314f
EloYoshida <helolondrina@gmail.com>
2020-03-30 21:30:25 -0300

    removendo overrides nao utilizados

--- src/Poligono.java
+++ src/Poligono.java
@@ -32,18 +32,6 @@ public abstract class Poligono extends Figura {
 	}
 
 
-	@Override
-	public double area() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
-
-
-	@Override
-	public double perimetro() {
-		// TODO Auto-generated method stub
-		return 0;
-	}
 
 	
 }
